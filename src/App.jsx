1ee5413c2022ed6fcf59523337b4c717cbf2d335
TAST1 <minhtuan7543@gmail.com>
2026-04-17 01:17:35 +0700

    invatation

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import CreateQR from './pages/CreateQR'
 import QrCode from './pages/QrCode'
+import ThanhYou from './components/ThanhYou'
 import './App.css'
 
 function App() {
@@ -24,6 +25,7 @@ function App() {
           />
           <Route path="/QrCode" element={<QrCode />} />
           <Route path="*" element={<Home />} />
+          <Route path="/thanh-you" element={<ThanhYou />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
