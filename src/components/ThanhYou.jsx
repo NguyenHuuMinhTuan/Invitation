49a35f5e5cd26b49dac72a4c04ed8c30ceff8f5e
TAST1 <minhtuan7543@gmail.com>
2026-04-17 01:15:35 +0700

    invatation

---
+++ src/components/ThanhYou.jsx
@@ -0,0 +1,12 @@
+const ThankYou = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>🎉 Cảm ơn bạn đã đồng ý tham dự!</h1>
+      <p>Hẹn gặp bạn tại lễ tốt nghiệp nha ❤️</p>
+      <p>Lưu ý nơi gửi xe: [Địa chỉ gửi xe] có thể gửi trong trường Đại học Gia Định ( kiến nghị nên gửi ở đây ), 2 là ngay hàm trung tâm nhà hàng adora</p>
+      <img src="../assets/park" alt="Nơi gửi xe" />
+    </div>
+  )
+}
+
+export default ThankYou
